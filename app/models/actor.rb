62755304fe072a004326b45c8af8851ad9ae4f0f
P. Michael Holland <maikeru.horando@gmail.com>
2013-04-09 21:39:33 +0900

    importing users from github

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'github/client'
+
 class Actor < ActiveRecord::Base
   has_many :events
+
+  # Apparently the leading :: in front of Github::Client
+  # specifies global scope. Without it ruby would look within the current scope
+  # i.e. Model::Actor::Github::Client in this case
+  def self.import name, klass = ::Github::Client
+    records = klass.fetch 'maikeru'
+
+    records.each do |record|
+      actor = Actor.find_or_create_by_name 'maikeru'
+      event = Event.create_from_record actor, record
+    end
+  end
 end
--- test/unit/actor_test.rb
+++ test/unit/actor_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ActorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_import
+    records = Github::Client.fetch 'maikeru'
+
+    assert_difference 'Actor.count', 1 do
+      assert_difference 'Event.count', records.length do
+        Actor.import 'maikeru', Github::Client
+      end
+    end
+  end
 end
